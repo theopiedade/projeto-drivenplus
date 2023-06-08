2655d5400f3b62386d8b306f63d38c973884a33c
Theo <theo.piedade@gmail.com>
2023-06-08 00:19:26 -0300

    Routes Folders and Pages Created

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import axios from 'axios';
+import Context from "./Context"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import SignUp from "./pages/SignUp/SignUp"
+import Plans from "./pages/Plans/Plans"
+import Users from "./pages/Users/Users"
+import './App.css'
+
+function App() {
+
+  const [userData, setUserData,] = useState([]);
+
+  axios.defaults.headers.common['Authorization'] = 'Y2VCJ9anYPua7XDEup8Q5oWn';
+
+  return (
+    <>
+    <Context.Provider value={[userData,setUserData]}>
+          <BrowserRouter>
+
+                <Routes>
+
+                  <Route path='/' element={  <Login/> } />
+                  <Route path='/sign-up' element={  <SignUp/> } />
+                  <Route path='/subscriptions/:idPlan' element={  <Plans/> } />
+                  <Route path='/subscriptions' element={  <Plans/> } />
+                  <Route path='/home' element={  <Home/> } />
+                  <Route path='/users' element={  <Users/> } />
+
+                </Routes>
+
+          </BrowserRouter>
+    </Context.Provider>
+    </>
+  )
+}
+
+export default App
