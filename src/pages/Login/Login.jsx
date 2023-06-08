9cd4ac200e7b0ed0acfa8c12d26f822cd26265f5
Theo <theo.piedade@gmail.com>
2023-06-08 02:18:13 -0300

    Styled Components init

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,55 @@
-export default function Login() {
-    
+import styled from "styled-components";
+export default function Login () {
+    return (
+           <ContainerApp>
+            <Logo>
+                <img src="../assets/driven.png"/>
+            </Logo>
+
+           </ContainerApp>
+      
+    )
 }
+
+
+const ContainerApp = styled.div`
+    width: 375px;
+    height: 667px;
+    background: #0E0E13;
+    display: flex;
+    flex-direction: row;
+    img {
+        margin-top: 134px;
+        width: 299px;
+        height: 49px;
+    }
+    input {
+        width: 299px;
+        height: 52px;
+        background: #FFFFFF;
+        border-radius: 8px;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 16px;
+        color: #7E7E7E;
+    }
+    button {
+    width: 298px;
+    height: 52px;
+    background: #FF4791;
+    border-radius: 8px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: #FFFFFF;
+  `
+
+  const Logo = styled.div`
+        margin-top: 134px;
+        width: 299px;
+        height: 49px;
+`
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,3 +1,45 @@
+import styled from "styled-components";
 export default function SignUp () {
-    
+    return (
+           <ContainerApp>
+             <img src="../assets/driven.png"/>
+           </ContainerApp>
+      
+    )
 }
+
+
+const ContainerApp = styled.div`
+    width: 375px;
+    height: 667px;
+    background: #0E0E13;
+    display: flex;
+    flex-direction: row;
+    img {
+    width: 299px;
+    height: 49px;
+    }
+    input {
+    width: 299px;
+    height: 52px;
+    background: #FFFFFF;
+    border-radius: 8px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #7E7E7E;
+    }
+    button {
+    width: 298px;
+    height: 52px;
+    background: #FF4791;
+    border-radius: 8px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: #FFFFFF;
+  `
